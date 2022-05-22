a9f40faad2647f561cf69603307d2ceaf80f9de1
yabukota <49775470+yabukota@users.noreply.github.com>
2022-05-22 13:23:18 +0800

    Delete src directory

--- src/main/java/com/example/accessingdatamysql/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.accessingdatamysql.service;
-import com.example.accessingdatamysql.domain.User;
-import java.util.List;
-
-public interface UserService {
-    List<User> listUsers();
-    List<User> listUsersByCondition(String user);
-}
